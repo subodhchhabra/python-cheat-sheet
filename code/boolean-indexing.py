c70ff97db08b22f187303fefeb3b2341d6d61c48
Julian Gaal <juliangaal@protonmail.com>
2017-03-05 23:26:54 +0100

    Seperated tricks into seperate file. More to come

---
+++ code/boolean-indexing.py
@@ -0,0 +1,21 @@
+# Just a short test file for playing around
+import numpy as np
+
+# Index trick when working with two np-arrays
+a = np.array([1, 2, 3, 6, 1, 4, 1])
+b = np.array([5, 6, 7, 8, 3, 1, 2])
+
+# Only saves a at index where b == 1
+other_a = a[b == 1]
+# Saves every spot in a except at index where b != 1
+other_other_a = a[b != 1]
+
+x = np.array([4, 6, 8, 1, 2, 6, 9])
+y = x > 5
+print(x[y])
+# >>> [6 8 6 9]
+
+# Even shorter
+x = np.array([1, 2, 3, 4, 4, 35, 212, 5, 5, 6])
+print(x[x < 5])
+# >>> [1 2 3 4 4]
